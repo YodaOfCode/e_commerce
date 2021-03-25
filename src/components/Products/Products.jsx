949556acc7f6f0c9688652bad32817bec99447ee
YodaOfCode <mmaxpokerr@gmail.com>
2021-03-25 19:08:28 +0200

    e_commerce version - 0.1.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from './styles'
 
 const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '100'},
-    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100'},
-    {id: 3, name: 'Shoes', description: 'Running shoes', price: '100'},
-    {id: 4, name: 'Shoes', description: 'Running shoes', price: '100'}
+    {id: 1, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
+    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
+    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
+    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
 ]
 
 const Products = () => {
+    const classes = useStyles();
+
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
-                    <Grid key={product.id} xs={12} sm={4} lg={6}>
+                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
                         <Product product={product}/>
                     </Grid>
                 ))}
