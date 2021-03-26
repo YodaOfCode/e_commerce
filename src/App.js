9464bb77d0b680d87bb30fab7039558aef76db78
ac88 <mmaxpokerr@gmail.com>
2021-03-26 19:04:15 +0200

    e_commerce version - 0.2.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-import {Navbar, Products} from './components';
+import {commerce} from "./components/lib/commerce";
+import {Navbar, Products, Cart} from './components';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const {data} = await commerce.products.list();
+
+        setProducts(data)
+    }
+
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+    };
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+        fetchCart();
+    }, [])
+
+
     return (
         <div>
-            <Navbar/>
-            <Products/>
+            <Navbar totalItems={cart.total_items}/>
+            {/*<Products products={products} onAddToCart={handleAddToCart}/>*/}
+            <Cart cart={cart}/>
         </div>
     );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,14 +3,7 @@ import {Grid} from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from './styles'
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-    {id: 2, name: 'Shoes', description: 'Running shoes', price: '100', image: 'https://images.unsplash.com/photo-1530878955558-a6c31b9c97db?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8bmljZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80'},
-]
-
-const Products = () => {
+const Products = ({products, onAddToCart}) => {
     const classes = useStyles();
 
     return (
@@ -19,7 +12,7 @@ const Products = () => {
             <Grid container justify='center' spacing={4}>
                 {products.map((product) => (
                     <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product}/>
+                        <Product product={product} onAddToCart={onAddToCart}/>
                     </Grid>
                 ))}
             </Grid>
